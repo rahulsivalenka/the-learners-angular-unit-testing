4b1926a722dd035711308c00b2ef2e017a5db1c4
Phani Rahul Sivalenka <rahul.sivalenka3@gmail.com>
2019-08-31 18:36:58 +0530

    add basic  unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,44 @@
-import { TestBed, async } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let de: DebugElement;
+  let component: AppComponent;
+
   beforeEach(async(() => {
+    console.log('beforeEach 1');
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   }));
 
+  beforeEach(() => {
+    console.log('beforeEach 2');
+    // Arrange
+    fixture = TestBed.createComponent(AppComponent);
+    de = fixture.debugElement;
+    component = de.componentInstance;
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    console.log('it 1');
+    // Assert
+    expect(component).toBeTruthy();
   });
 
   it(`should have as title 'angular-unit-testing'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-unit-testing');
+    console.log('it 2');
+    expect(component.title).toEqual('angular-unit-testing');
   });
 
   it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    console.log('it 3');
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-unit-testing!');
+    const compiled = de.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain(
+      'Welcome to angular-unit-testing!'
+    );
   });
 });
